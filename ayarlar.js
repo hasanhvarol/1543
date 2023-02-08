f1e8a7287272f3a26ca9e4fb7bc53cd5a29bae66
hasanhvarol <48387559+hasanhvarol@users.noreply.github.com>
2023-02-08 15:20:00 +0300

    Add files via upload

---
+++ ayarlar.js
@@ -0,0 +1,51 @@
+﻿module.exports = {
+
+    token: "MTA3MTc0Mzc2MzIwODIyMDcwNA.GilfoX.vcz3cXLSWnZXvTTh7GzJOEZIsLq2rqwfErH8Qg",
+    prefix: ".",
+    sahip: "995174121178681446",
+    durum: "Sea",
+
+    api: {
+        TCKN: "http://localhost/zirvedebiraktik/tc.php?auth_token=qwertyzz&",
+        TC_GSM: "http://localhost/zirvedebiraktik/tcgsm.php?auth_token=qwertyzz&",
+        GSM_TC: "http://localhost/zirvedebiraktik/gsmtc.php?auth_token=qwertyzz&",
+        AD_SOYAD: "http://localhost/zirvedebiraktik/adsoyad.php?auth_token=qwertyzz&",
+        AILEA: "localhost/zirvedebiraktik/soyagaci.php?qwertyzz&",
+        AILEB: "localhost/zirvedebiraktik/soyagaci2.php?qwertyzz&",
+        DDOS: "",
+        SECMEN_TCKN: "",
+        SECMEN_AD_SOYAD: "",
+        AOL: "",
+        ASI: "",
+        AD: "http://localhost/zirvedebiraktik/ad.php?auth_token=qwertyzz&",
+        SOYAD: "http://localhost/zirvedebiraktik/soyad.php?auth_token=qwertyzz&",
+        IKAMETGAH: ""
+    },
+
+    emojiler: {
+        UPLOAD: "<:evilinkralicesi:1044987516278812713>"
+    },
+
+    roller: {
+        admin: "1071766071641649203",
+        vip: "1072604621970870312",
+        premium: "1072604894919397538",
+        booster: "1072604733673582622",
+        limitlirol: "1064205051628888185",
+        freemium: "",
+        yardimci: "1065721276909502564"
+    },
+
+    log: "",
+    log2: "",
+
+    wl: [""], // log tutmaz - rowy farkı ;)
+
+    sunucuID: "1071412674090520617",
+    kanal: ["1071753933887897641"],
+    seskanal: "1071412674090520621",
+
+    sorgulimit: "600",
+
+    bakim: ""
+}
